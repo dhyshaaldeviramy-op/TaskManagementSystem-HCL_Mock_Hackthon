14c58bef762b480ae6500622cbc4220558c1ac10
Dhyshaal Devira <dhyshaaldevira@gmail.com>
2026-04-16 22:43:58 +0530

    backend part

---
+++ TaskManagementSystemAsp/Controllers/TasksController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TaskManagementSystemAsp.Data;
+using TaskManagementSystemAsp.Models;
+
+namespace TaskManagementSystemAsp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TasksController : ControllerBase
+    {
+
+       
+        private readonly AppDbContext _context;
+
+        public TasksController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetTasks()
+        {
+            return Ok(await _context.Tasks.ToListAsync());
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddTask(TaskItem task)
+        {
+            _context.Tasks.Add(task);
+            await _context.SaveChangesAsync();
+            return Ok(task);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateTask(int id, TaskItem task)
+        {
+            var existing = await _context.Tasks.FindAsync(id);
+            if (existing == null) return NotFound();
+
+            existing.Title = task.Title;
+            existing.Description = task.Description;
+            existing.IsCompleted = task.IsCompleted;
+
+            await _context.SaveChangesAsync();
+            return Ok(existing);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteTask(int id)
+        {
+            var task = await _context.Tasks.FindAsync(id);
+            if (task == null) return NotFound();
+
+            _context.Tasks.Remove(task);
+            await _context.SaveChangesAsync();
+            return Ok();
+        }
+    }
+}
+
---
+++ TaskManagementSystemAsp/Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TaskManagementSystemAsp.Models;
+
+namespace TaskManagementSystemAsp.Data
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<TaskItem> Tasks { get; set; }
+    }
+}
