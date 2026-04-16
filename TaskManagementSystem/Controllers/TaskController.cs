0ace80368b30376cd8fbe5f5739ee9ca6a716323
Dhyshaal Devira <dhyshaaldevira@gmail.com>
2026-04-16 20:36:05 +0530

    Use Case done using MVC

---
+++ TaskManagementSystem/Controllers/TaskController.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TaskManagementSystem.Data;
+using TaskManagementSystem.Models;
+
+namespace TaskManagementSystem.Controllers
+{
+    public class TaskController : Controller
+    {
+    
+        private readonly AppDbContext _context;
+
+        public TaskController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Tasks
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Tasks.ToListAsync());
+        }
+
+        // GET: Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Create
+        [HttpPost]
+        public async Task<IActionResult> Create(TaskItem task)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(task);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(task);
+        }
+
+        // GET: Edit
+        public async Task<IActionResult> Edit(int id)
+        {
+            var task = await _context.Tasks.FindAsync(id);
+            return View(task);
+        }
+
+        // POST: Edit
+        [HttpPost]
+        public async Task<IActionResult> Edit(int id, TaskItem task)
+        {
+            if (id != task.Id) return NotFound();
+
+            if (ModelState.IsValid)
+            {
+                _context.Update(task);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(task);
+        }
+
+        // DELETE
+        public async Task<IActionResult> Delete(int id)
+        {
+            var task = await _context.Tasks.FindAsync(id);
+            _context.Tasks.Remove(task);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        // Toggle Status
+        public async Task<IActionResult> ToggleStatus(int id)
+        {
+            var task = await _context.Tasks.FindAsync(id);
+            task.IsCompleted = !task.IsCompleted;
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
+
